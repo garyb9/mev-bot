b58b5c3a4873d765a2a1160bc86b2e1e2445e003
garyb9 <gberkinfand@gmail.com>
2023-05-05 10:42:48 +0300

    adding provider

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
+// not for production!
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use std::env;
+use ethers::prelude::*;
 
-const FLASHBOTS_BUNLDE_RELAY_URL: &str = "https://relay-goerli.flashbots.net";
-
-fn main() {
+// const FLASHBOTS_BUNLDE_RELAY_URL: &str = "https://relay-goerli.flashbots.net";
+#[tokio::main]
+async fn main() {
     dotenv::dotenv().ok();
-    println!("{}", FLASHBOTS_BUNLDE_RELAY_URL);
+
+    let goerli_rpc: String = env::var("GOERLI_RPC_URL").expect("GOERLI_RPC_URL not set");
+
+    let provider: Provider<Http> =
+        Provider::<Http>::try_from(goerli_rpc).expect("could not instantiate HTTP Provider");
+
+    let block: Block<Transaction> = provider
+        .get_block_with_txs(BlockNumber::Latest)
+        .await
+        .unwrap()
+        .unwrap();
+
+    println!("Block {}", serde_json::to_string_pretty(&block).unwrap());
+
+    // let private_key: String = env::var("PRIVATE_KEY").expect("PRIVATE_KEY not set");
+    // let flashbots_key: String = env::var("FLASHBOTS_KEY").expect("FLASHBOTS_KEY not set");
+
+    // let wallet = Wallet::new(&mut &private_key);
+
+    // Print the wallet address
+    // println!("Wallet address: {}", wallet.address());
+
+    // Ok(())
 }
