6b5b1b43ff8bca71e5d1883609531337ade3cfdf
garyb9 <gberkinfand@gmail.com>
2023-05-06 03:22:05 +0300

    new blocks baseFeePerGas

--- src/listener.rs
+++ src/listener.rs
@@ -11,15 +11,21 @@ impl BlockListener {
     }
 
     pub async fn listen(&self) -> anyhow::Result<()> {
+        println!("Listening to incoming blocks");
         let stream: FilterWatcher<Http, TxHash> = self.provider.watch_blocks().await?;
         tokio::pin!(stream);
         stream
             .for_each(|block_hash| async move {
                 let block = self.provider.get_block(block_hash).await.unwrap().unwrap();
+                // println!(
+                //     "Transactions in Block {}: {}",
+                //     block.number.unwrap(),
+                //     serde_json::to_string_pretty(&(block.transactions)).unwrap()
+                // );
                 println!(
-                    "Transactions in Block {}: {}",
+                    "Block {} -> baseFeePerGas: {} Gwei",
                     block.number.unwrap(),
-                    serde_json::to_string_pretty(&(block.transactions)).unwrap()
+                    block.base_fee_per_gas.unwrap() / 1_000_000_000
                 );
             })
             .await;
